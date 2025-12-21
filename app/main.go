8195f4762371037ab8218dce2407baea7869d1fc
mviswanathsai <mviswanath.sai.met21@itbhu.ac.in>
2025-12-21 21:37:31 +0530

    add hash-object command
    fix zlib writer bug
    
    update dir creation err handling
    
    improve error handling
    
    fix formatting directives
    
    fix formatting directives
    
    fix formatting directives

--- app/main.go
+++ app/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"bufio"
 	"compress/zlib"
+	"crypto/sha1"
+	"errors"
 	"fmt"
 	"io"
+	"io/fs"
 	"os"
 	fp "path/filepath"
 )
 
 const (
-	objdir         = ".git/objects"
+	gitObjDir      = ".git/objects"
 	objheaderdelim = 0
 )
 
@@ -45,26 +48,92 @@ func main() {
 		blobname := objhash[2:]
 
 		// we first need to open the file
-		filename := fp.Join(objdir, blobdir, blobname)
+		filename := fp.Join(gitObjDir, blobdir, blobname)
 		f, err := os.Open(filename)
 		if err != nil {
-			fmt.Print(err)
-			return
+			fmt.Fprintf(os.Stderr, "Error opening file: %v\n", err)
+			os.Exit(1)
 		}
 
 		zr, err := zlib.NewReader(f)
-		defer zr.Close()
 		if err != nil {
-			fmt.Print(err)
-			return
+			fmt.Fprintf(os.Stderr, "Error decompressing file: %v\n", err)
+			os.Exit(1)
 		}
+		defer zr.Close()
 
 		r := bufio.NewReader(zr)
 		r.ReadBytes(objheaderdelim)
 		io.Copy(os.Stdout, r)
 
+	case "hash-object":
+		var filename string
+		if len(os.Args) == 3 {
+			filename = os.Args[2]
+		} else if len(os.Args) == 4 {
+			if os.Args[2] == "-w" {
+				filename = os.Args[3]
+			} else {
+				fmt.Fprintf(os.Stderr, "Unknown flag %s for command hash-object\n", os.Args[2])
+				os.Exit(1)
+			}
+		}
+
+		// Read file and get hash
+		d, err := os.ReadFile(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %v\n", err)
+			os.Exit(1)
+		}
+		c := fmt.Sprintf("blob %d\x00%s", len(d), d)
+
+		hash := sha1.New()
+		io.WriteString(hash, c)
+		h := fmt.Sprintf("%x", hash.Sum(nil))
+
+		if len(os.Args) == 3 {
+			fmt.Println(h)
+			return
+		}
+
+		objDirName, objFileName := pathFromHash(h)
+		err = os.Mkdir(fp.Join(gitObjDir, objDirName), 0755)
+		if err != nil && !errors.Is(err, fs.ErrExist) {
+			fmt.Fprintf(os.Stderr, "Error creating dir: %v\n", err)
+			os.Exit(1)
+		}
+
+		objFilePath := fp.Join(gitObjDir, objDirName, objFileName)
+		f, err := os.Create(objFilePath)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating file: %v\n", err)
+			os.Exit(1)
+		}
+		defer f.Close()
+
+		// Compress file
+		w := zlib.NewWriter(f)
+		_, err = w.Write([]byte(c))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error compressing file: %v\n", err)
+			os.Exit(1)
+		}
+		w.Close()
+		fmt.Print(h)
+
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
 	}
+}
+
+func check(err error) {
+	if err != nil {
+		fmt.Print(err)
+		os.Exit(1)
+	}
+}
+
+func pathFromHash(hash string) (dirName, objName string) {
+	return hash[:2], hash[2:]
 }
